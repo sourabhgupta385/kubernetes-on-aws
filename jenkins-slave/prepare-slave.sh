9fbd04f64fe2cd81e9d9d6c20a88136ffe6d5f49
sourabhgupta385 <gupta.sourabh385@gmail.com>
2020-12-17 16:16:32 +0530

    added command to change ownership

--- jenkins-slave/prepare-slave.sh
+++ jenkins-slave/prepare-slave.sh
@@ -1,11 +1,11 @@
-# Install openjdk 8
+# Put private Keys for Kubernetes Infra in /home/ubuntu/k8s-infra.pem
 
+# Install openjdk 8
 sudo apt-get update
 sudo apt-get install -y openjdk-8-jdk
 
 
 # Install Terraform
-
 wget https://releases.hashicorp.com/terraform/0.14.2/terraform_0.14.2_linux_amd64.zip
 sudo apt install -y unzip
 unzip terraform_0.14.2_linux_amd64.zip
@@ -13,7 +13,6 @@ sudo mv terraform /usr/bin
 
 
 # Install Ansible
-
 sudo apt update
 sudo apt install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
@@ -25,11 +24,9 @@ terraform version
 ansible --version
 
 # Create directory to store Jenkins data
-
 mkdir jenkins-data
 
 # Create directory to store ssh config files
-
 mkdir ~/.ssh/include
 
 # Create SSH config file in ~/.ssh
@@ -37,5 +34,5 @@ cat <<EOF >> /home/ubuntu/.ssh/config
 Include ~/.ssh/include/*
 EOF
 
-# Put private Keys for Kubernetes Infra in /home/ubuntu/k8s-infra.pem
-# Change the permission of pem file by executing chmod 400 /home/ubuntu/k8s-infra.pem
+# Change the permission of pem file by executing 
+chmod 400 /home/ubuntu/k8s-infra.pem
