99dd07b72b3d99b0964ab90fa06fbd36c6651bfb
sourabhgupta385 <gupta.sourabh385@gmail.com>
2020-12-29 19:24:05 +0530

    rectifying error in inventory generation

--- k8s-infra/main.tf
+++ k8s-infra/main.tf
@@ -33,8 +33,8 @@ data "template_file" "inventory" {
     public_ip_address_bastion = join("\n", formatlist("bastion ansible_host=%s", module.virtual-machines.bastion_public_ip))
     connection_strings_master = join("\n", formatlist("master1 ansible_host=%s",  module.virtual-machines.k8s_master_private_ip))
     connection_strings_worker = join("\n", formatlist("worker1 ansible_host=%s",  module.virtual-machines.k8s_worker_private_ip))
-    list_master               = join("\n", "master1")
-    list_worker               = join("\n", "worker1")
+    list_master               = join("\n", ["master1"])
+    list_worker               = join("\n", ["worker1"])
   }
 }
 
